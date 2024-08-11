e0567aa3f0d1b08a5b3b1cd9cc8e0450ac6cd8d2
Hantaizone <sakibquaisar696@gmail.com>
2024-08-12 01:06:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,7 +88,7 @@ async def start_command(client: Client, message: Message):
     
     [
                     InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 MAIN CHANNEL', url='https://t.me/Anime_Sub_society')
+                    InlineKeyboardButton('🍁 MAIN CHANNEL', url='https://t.me/h3ntai_nation')
         
     ]
             ]
